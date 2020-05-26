b439082a258a5491e40db2e4ea4950ce9d595043
Sharon Stratsianis <SharonStrats@users.noreply.github.com>
2020-05-26 19:14:20 +1000

    Update src/contexts/UserContext.ts
    Co-authored-by: Vincent <Vinnl@users.noreply.github.com>

--- src/contexts/UserContext.ts
+++ src/contexts/UserContext.ts
@@ -1,3 +1,12 @@
 import { createContext } from 'react';
+// Not sure if this is the correct interface:
+import { Session } from '@inrupt/solid-auth-fetcher';
+import { IriString } from 'lit-solid';
 
-export default createContext({});
+interface Context  {
+    session: Session | undefined;
+    isLoading: boolean | undefined;
+    resources: IriString[] | undefined;
+}
+
+export const UserContext = createContext<Context>({});
