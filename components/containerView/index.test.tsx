9ac2f279734da504fdb638c8477f6a35fc8be34b
Vincent <git@vincenttunru.com>
2020-06-03 09:59:59 +0200

    Remove explicit casts in tests
    Note that the actual functions' implementations are still typed as
    `any`, but at least it's clear that it's a mock.

--- components/containerView/index.test.tsx
+++ components/containerView/index.test.tsx
@@ -2,8 +2,6 @@ import { shallow } from "enzyme";
 import { shallowToJson } from "enzyme-to-json";
 import { mock } from "jest-mock-extended";
 
-import { getIriAll } from "lit-solid";
-import { getSession } from "../../lib/solid-auth-fetcher/dist";
 import ContainerView, { getContainerResourceIrisFromSession } from "./index";
 import { ILoggedInSolidSession } from "../../lib/solid-auth-fetcher/dist/solidSession/ISolidSession";
 
@@ -38,14 +36,16 @@ describe("Loading container resource iri list", () => {
     // Disable this rule to allow mocks. Not sure of a better way.
     /* eslint @typescript-eslint/no-explicit-any: 0 */
 
-    (getSession as any).mockImplementation(() =>
+    const getSession: jest.Mock = jest.requireMock("../../lib/solid-auth-fetcher/dist").getSession;
+    getSession.mockImplementation(() =>
       Promise.resolve({
         webId: "https://myaccount.mypodserver.com/profile/card#me",
       })
     );
 
-    (getIriAll as any).mockImplementationOnce(() => [containerIri]);
-    (getIriAll as any).mockImplementationOnce(() => resources);
+    const getIriAll: jest.Mock = jest.requireMock('lit-solid').getIriAll;
+    getIriAll.mockImplementationOnce(() => [containerIri]);
+    getIriAll.mockImplementationOnce(() => resources);
 
     expect(await getContainerResourceIrisFromSession(session)).toEqual({
       containerIri,
