e88defcabd29c8e3512724efd6e322d3a4386405
Arne Hassel <arne.hassel@gmail.com>
2020-08-31 18:04:16 +0200

    Removing the unique ids that are generated for JSS classnames (#67)
    This should make the output of snapshots more consistent wrt classnames

--- __testUtils/mountWithTheme.tsx
+++ __testUtils/mountWithTheme.tsx
@@ -19,20 +19,25 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-import { ReactNode, ReactElement } from "react";
-import { ThemeProvider } from "@material-ui/styles";
+import React, { ReactNode, ReactElement } from "react";
 import { mount } from "enzyme";
 import { mountToJson as enzymeMountToJson } from "enzyme-to-json";
+import { PrismTheme } from "@solid/lit-prism-patterns";
+import { StylesProvider, ThemeProvider } from "@inrupt/prism-react-components";
 import defaultTheme from "../src/theme";
 
 interface IWithTheme {
-  theme?: any;
+  theme?: PrismTheme;
   children: ReactNode;
 }
 
 export const WithTheme = (props: IWithTheme): ReactElement => {
   const { theme, children } = props;
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <StylesProvider generateClassName={(rule) => `PodBrowser-${rule.key}`}>
+      <ThemeProvider theme={theme!}>{children}</ThemeProvider>
+    </StylesProvider>
+  );
 };
 
 WithTheme.defaultProps = {
@@ -42,7 +47,7 @@ WithTheme.defaultProps = {
 export const mountToJson = (
   children: ReactNode,
   theme = defaultTheme
-): object => {
+): unknown => {
   const tree = mount(<WithTheme theme={theme}>{children}</WithTheme>);
   return enzymeMountToJson(tree);
 };
