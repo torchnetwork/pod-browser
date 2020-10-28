0e5cc159934c90194afab1258faf66f83f43bddb
Arne Hassel <arne.hassel@gmail.com>
2020-10-28 16:47:53 +0100

    Update index.jsx

--- components/addContact/index.jsx
+++ components/addContact/index.jsx
@@ -23,7 +23,7 @@ import React, { useContext, useState } from "react";
 import { createStyles, makeStyles } from "@material-ui/styles";
 import { Box } from "@material-ui/core";
 import { useBem } from "@solid/lit-prism-patterns";
-import { BackToNavLink } from "@inrupt/prism-react-components";
+import { BackToNav, BackToNavLink } from "@inrupt/prism-react-components";
 import clsx from "clsx";
 import Link from "next/link";
 import { getSourceUrl } from "@inrupt/solid-client";
@@ -125,14 +125,16 @@ export default function AddContact() {
   });
 
   const handleChange = (newValue) => setAgentId(newValue);
+  
+  const link = (
+    <Link href="/contacts" replace passHref>
+      <BackToNavLink>contacts</BackToNavLink>
+    </Link>
+  );
 
   return (
     <div className={containerClass}>
-      <Box mt={2} mb={2}>
-        <Link href="/contacts" replace passHref>
-          <BackToNavLink>contacts</BackToNavLink>
-        </Link>
-      </Box>
+      <BackToNav link={link} />
 
       <h3>Add new contact</h3>
 
